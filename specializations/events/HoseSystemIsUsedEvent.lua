8c27d1291420786d81b1260aeb955c0bad5be002
Stijn Wopereis <stijnwop@gmail.com>
2017-05-02 22:26:24 +0200

    Updated isUsed Event

--- specializations/events/HoseSystemIsUsedEvent.lua
+++ specializations/events/HoseSystemIsUsedEvent.lua
@@ -16,12 +16,13 @@ function HoseSystemIsUsedEvent:emptyNew()
     return event
 end
 
-function HoseSystemIsUsedEvent:new(object, index, isConnected)
+function HoseSystemIsUsedEvent:new(object, index, isConnected, isCalledFromReference)
     local event = HoseSystemIsUsedEvent:emptyNew()
 
     event.object = object
     event.index = index
     event.isConnected = isConnected
+    event.isCalledFromReference = isCalledFromReference
 
     return self
 end
@@ -30,29 +31,31 @@ function HoseSystemIsUsedEvent:writeStream(streamId, connection)
     writeNetworkNodeObject(streamId, self.object)
     streamWriteInt32(streamId, self.index)
     streamWriteBool(streamId, self.isConnected)
+    streamWriteBool(streamId, self.isCalledFromReference)
 end
 
 function HoseSystemIsUsedEvent:readStream(streamId, connection)
     self.object = readNetworkNodeObject(streamId)
     self.index = streamReadInt32(streamId)
     self.isConnected = streamReadBool(streamId)
+    self.isCalledFromReference = streamReadBool(streamId)
     self:run(connection)
 end
 
 function HoseSystemIsUsedEvent:run(connection)
-	self.object:setIsUsed(self.index, self.isConnected, true)
+	self.object:setIsUsed(self.index, self.isConnected, self.isCalledFromReference, true)
 
     if not connection:getIsServer() then
         g_server:broadcastEvent(self, false, connection, self.object)
     end
 end
 
-function HoseSystemIsUsedEvent.sendEvent(object, index, isConnected, noEventSend)
+function HoseSystemIsUsedEvent.sendEvent(object, index, isConnected, isCalledFromReference, noEventSend)
     if noEventSend == nil or noEventSend == false then
         if g_server ~= nil then
-            g_server:broadcastEvent(HoseSystemIsUsedEvent:new(object, index, isConnected), nil, nil, object)
+            g_server:broadcastEvent(HoseSystemIsUsedEvent:new(object, index, isConnected, isCalledFromReference), nil, nil, object)
         else
-            g_client:getServerConnection():sendEvent(HoseSystemIsUsedEvent:new(object, index, isConnected))
+            g_client:getServerConnection():sendEvent(HoseSystemIsUsedEvent:new(object, index, isConnected, isCalledFromReference))
         end
     end
 end
