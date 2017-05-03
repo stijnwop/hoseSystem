5857904c59b158417671de81cd158e1b8e84be29
Stijn Wopereis <stijnwop@gmail.com>
2017-05-03 23:45:29 +0200

    Reference search optimization and getAllowDetach tweak

--- specializations/HoseSystemReferences.lua
+++ specializations/HoseSystemReferences.lua
@@ -124,49 +124,51 @@ function HoseSystemReferences:searchReferences(grabPoint)
     local x, y, z = getWorldTranslation(grabPoint.node)
     local sequence = 0.6 * 0.6
 
-    if g_currentMission.hoseSystemReferences ~= nil and #g_currentMission.hoseSystemReferences > 0 then
-        for _, hoseSystemReference in pairs(g_currentMission.hoseSystemReferences) do
-            -- Hose references
-            if hoseSystemReference ~= nil then
-                for i, reference in pairs(hoseSystemReference.hoseSystemReferences) do
-                    if not reference.isUsed then
-                        if HoseSystemReferences:getCanConnect(x, y, z, sequence, grabPoint, reference) then
-                            -- self.inRangeVehicle = reference.isObject and g_currentMission:getNodeObject(liquidManureHoseReference.nodeId) or liquidManureHoseReference -- getNodeObject does work in MP?
-                            -- self.inRangeReference = reference.id
-                            -- self.inRangeIsExtendable = false
+    if not grabPoint.connectable then
+        if g_currentMission.hoseSystemReferences ~= nil and #g_currentMission.hoseSystemReferences > 0 then
+            for _, hoseSystemReference in pairs(g_currentMission.hoseSystemReferences) do
+                -- Hose references
+                if hoseSystemReference ~= nil then
+                    for i, reference in pairs(hoseSystemReference.hoseSystemReferences) do
+                        if not reference.isUsed then
+                            if HoseSystemReferences:getCanConnect(x, y, z, sequence, grabPoint, reference) then
+                                -- self.inRangeVehicle = reference.isObject and g_currentMission:getNodeObject(liquidManureHoseReference.nodeId) or liquidManureHoseReference -- getNodeObject does work in MP?
+                                -- self.inRangeReference = reference.id
+                                -- self.inRangeIsExtendable = false
 
-                            -- self:loadFillableObjectAndReference(reference.isObject and g_currentMission:getNodeObject(liquidManureHoseReference.nodeId) or liquidManureHoseReference, reference.id, false)
-                            -- local object = reference.isObject and g_currentMission:getNodeObject(liquidManureHoseReference.nodeId) or liquidManureHoseReference
-                            local object = reference.isObject and hoseSystemReference.fillLevelObject or hoseSystemReference
-                            self:loadFillableObjectAndReference(networkGetObjectId(object), i, false)
-                            break
+                                -- self:loadFillableObjectAndReference(reference.isObject and g_currentMission:getNodeObject(liquidManureHoseReference.nodeId) or liquidManureHoseReference, reference.id, false)
+                                -- local object = reference.isObject and g_currentMission:getNodeObject(liquidManureHoseReference.nodeId) or liquidManureHoseReference
+                                local object = reference.isObject and hoseSystemReference.fillLevelObject or hoseSystemReference
+                                self:loadFillableObjectAndReference(networkGetObjectId(object), i, false)
+                                break
+                            end
                         end
                     end
                 end
             end
         end
-    end
+    else
+        if g_currentMission.hoseSystemHoses ~= nil and #g_currentMission.hoseSystemHoses > 0 then
+            for _, hoseSystemHose in pairs(g_currentMission.hoseSystemHoses) do
+                if hoseSystemHose ~= self and hoseSystemHose.grabPoints ~= nil then
+                    for i, reference in pairs(hoseSystemHose.grabPoints) do
+                        if grabPoint.connectable or reference.connectable then
+                            if HoseSystem:getIsDetached(reference.state) then
+                                local rx, ry, rz = getWorldTranslation(reference.node)
+                                local dist = Utils.vector2LengthSq(x - rx, z - rz)
 
-    if g_currentMission.hoseSystemHoses ~= nil and #g_currentMission.hoseSystemHoses > 0 then
-        for _, hoseSystemHose in pairs(g_currentMission.hoseSystemHoses) do
-            if hoseSystemHose ~= self and hoseSystemHose.grabPoints ~= nil then
-                for i, reference in pairs(hoseSystemHose.grabPoints) do
-                    if grabPoint.connectable or reference.connectable then
-                        if HoseSystem:getIsDetached(reference.state) then
-                            local rx, ry, rz = getWorldTranslation(reference.node)
-                            local dist = Utils.vector2LengthSq(x - rx, z - rz)
+                                if dist < sequence then
+                                    local vehicleDistance = math.abs(y - ry)
 
-                            if dist < sequence then
-                                local vehicleDistance = math.abs(y - ry)
-
-                                if vehicleDistance < 1.3 then
-                                    if HoseSystemReferences:getCanExtend(reference.id > 1, reference.node, grabPoint.node) then
-                                        self:loadFillableObjectAndReference(networkGetObjectId(hoseSystemHose), i, true)
-                                        sequence = dist
-                                        -- self.inRangeVehicle = liquidManureHose
-                                        -- self.inRangeReference = reference.id
-                                        -- self.inRangeIsExtendable = true
-                                        break
+                                    if vehicleDistance < 1.3 then
+                                        if HoseSystemReferences:getCanExtend(reference.id > 1, reference.node, grabPoint.node) then
+                                            self:loadFillableObjectAndReference(networkGetObjectId(hoseSystemHose), i, reference.connectable)
+                                            sequence = dist
+                                            -- self.inRangeVehicle = liquidManureHose
+                                            -- self.inRangeReference = reference.id
+                                            -- self.inRangeIsExtendable = true
+                                            break
+                                        end
                                     end
                                 end
                             end
@@ -247,12 +249,14 @@ function HoseSystemReferences:getAllowsDetach(object, index)
             local reference = HoseSystemReferences:getReference(grabPoint.connectorVehicle, grabPoint.connectorRefId, grabPoint)
 
             if reference ~= nil then
-                if reference.flowOpened or reference.isLocked then
+                if reference.flowOpened or reference.isLocked or reference.connectable then
                     return false
                 end
+
+                return true
             end
         end
     end
 
-    return true
+    return false
 end
