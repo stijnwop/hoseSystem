07ff79eb0ebb77cb4fcc262ce00a5b6b56697de9
Stijn Wopereis <stijnwop@gmail.com>
2017-09-05 20:18:34 +0200

    Util updated to avoid code duplication

--- specializations/HoseSystemUtil.lua
+++ specializations/HoseSystemUtil.lua
@@ -10,8 +10,93 @@ HoseSystemUtil = {
     consoleCommandToggleHoseSystemDebugRendering = function(unusedSelf)
         HoseSystem.debugRendering = not HoseSystem.debugRendering
 
-        return "HoseSystemDebugRendering = "..tostring(HoseSystem.debugRendering)
+        return "HoseSystemDebugRendering = " .. tostring(HoseSystem.debugRendering)
     end
 }
+
+---
+-- @param vehicle
+--
+function HoseSystemUtil:addToPhysicsRecursively(vehicle)
+    if vehicle ~= nil then
+        vehicle:addToPhysics()
+
+        HoseSystemUtil:addToPhysicsRecursively(vehicle.attacherVehicle)
+    end
+end
+
+---
+-- @param vehicle
+--
+function HoseSystemUtil:removeFromPhysicsRecursively(vehicle)
+    if vehicle ~= nil then
+        vehicle:removeFromPhysics()
+
+        HoseSystemUtil:removeFromPhysicsRecursively(vehicle.attacherVehicle)
+    end
+end
+
+---
+-- @param reference
+--
+function HoseSystemUtil:addHoseSystemToPhysics(grabPoint, reference)
+    local hoseSystem = HoseSystemUtil:getHoseSystemFromReference(reference)
+
+    if hoseSystem ~= nil then
+        hoseSystem.poly.interactiveHandling:addToPhysicsParts(grabPoint, {}, grabPoint.connectorVehicle, reference, true)
+    end
+end
+
+---
+-- @param reference
+--
+function HoseSystemUtil:removeHoseSystemFromPhysics(reference)
+    local hoseSystem = HoseSystemUtil:getHoseSystemFromReference(reference)
+
+    if hoseSystem ~= nil then
+        hoseSystem:removeFromPhysics()
+    end
+end
+
+---
+-- @param object
+-- @param referenceId
+--
+function HoseSystemUtil:getReferencesWithSingleConnection(object, referenceId)
+    local references = {}
+
+    for id, reference in pairs(object.hoseSystemReferences) do
+        local continue = true
+
+        if referenceId ~= nil and referenceId == id then
+            continue = false
+        end
+
+        if continue then
+            if reference.isUsed and not reference.parkable then
+                if reference.hoseSystem ~= nil and reference.hoseSystem.grabPoints ~= nil then
+                    local grabPoints = HoseSystem:getConnectedReferenceGrabPoints(reference.hoseSystem, id)
+
+                    if #grabPoints == 1 then
+                        table.insert(references, { grabPoint = grabPoints[1], reference = reference })
+                    end
+                end
+            end
+        end
+    end
+
+    return references
+end
+
+---
+-- @param reference
+--
+function HoseSystemUtil:getHoseSystemFromReference(reference)
+    if reference.hoseSystem ~= nil then
+        return reference.hoseSystem
+    end
+
+    return nil
+end
 
 addConsoleCommand("gsToggleHoseSystemDebugRendering", "Toggles the debug rendering of the HoseSystem", "consoleCommandToggleHoseSystemDebugRendering", HoseSystemUtil)
