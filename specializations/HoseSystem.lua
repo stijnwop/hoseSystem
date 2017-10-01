93cdbc9c3d31c4aa07e7751db62d2e3d78df19cf
Stijn Wopereis <stijnwop@gmail.com>
2017-10-01 14:32:47 +0200

    Disable default logging

--- specializations/HoseSystem.lua
+++ specializations/HoseSystem.lua
@@ -7,8 +7,8 @@
 -- Copyright (c) Wopster, 2017
 
 HoseSystem = {
-    debugRendering = true,
-    logLevel = 3,
+    debugRendering = false,
+    logLevel = 2,
     baseDirectory = g_currentModDirectory
 }
 
