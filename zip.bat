180f72bb1e6c372fc60183b3580860bee40463e7
Stijn Wopereis <stijnwop@gmail.com>
2018-04-06 12:07:29 +0200

    Update zip batch to exclude tests

--- zip.bat
+++ zip.bat
@@ -6,4 +6,4 @@ if exist "%ProgramFiles%\7-Zip" (
   set zipRoot="%ProgramFiles%\7-Zip"
 ) 
 
-%zipRoot%\7z.exe a -tzip "FS17_hoseSystem_dev.zip" "*.xml" "*.lua" "*.i3d" "*.i3d.shapes" "*.dds" -r -xr!_modding  -xr!data
+%zipRoot%\7z.exe a -tzip "FS17_hoseSystem_dev.zip" "*.xml" "*.lua" "*.i3d" "*.i3d.shapes" "*.dds" -r -xr!_modding  -xr!data -xr!tests -xr!".idea" -xr!run_unit_tests.lua
